1f225fe633cc605bab251dc994b1fec85adb1ecb
cipherувг-uz <ciphereduuz@gmail.com>
2022-12-20 23:40:07 +0500

    adding files

--- core/views.py
+++ core/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+    context = {
+        "posts":posts
+    }
+
+    return render(request, 'index.html', context)
+
+
+
 def blog(request):
     return render(request, 'blog.html')
