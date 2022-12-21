1a6502f6264a0a328c8d3f957be96bfae69e18cd
cipherувг-uz <ciphereduuz@gmail.com>
2022-12-21 21:36:10 +0500

    adding media files

--- core/models.py
+++ core/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=256)
-
+    rasm = models.ImageField(upload_to="rasmlar/")
     def __str__(self):
         return self.title
--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import *
-
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
